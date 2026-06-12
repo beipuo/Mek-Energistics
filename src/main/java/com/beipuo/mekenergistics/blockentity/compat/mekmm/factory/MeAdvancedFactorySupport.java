575da1301f28ac391e015a44e068eff355d3d6b5
beipuo <beipuo31142@outlook.com>
2026-06-12 22:51:22 +0800

    Simplify factory compat owner bridges

--- src/main/java/com/beipuo/mekenergistics/blockentity/compat/mekmm/factory/MeAdvancedFactorySupport.java
+++ src/main/java/com/beipuo/mekenergistics/blockentity/compat/mekmm/factory/MeAdvancedFactorySupport.java
@@ -4,7 +4,6 @@ import com.beipuo.mekenergistics.blockentity.compat.shared.MeExternalFactorySupp
 import appeng.api.crafting.IPatternDetails;
 import appeng.api.stacks.KeyCounter;
 import com.beipuo.mekenergistics.blockentity.support.MeFactoryAeSupport;
-import com.beipuo.mekenergistics.common.machine.MeMekanismMachine;
 import java.util.Collections;
 import java.util.List;
 import mekanism.api.IContentsListener;
@@ -15,94 +14,75 @@ import mekanism.common.capabilities.energy.MachineEnergyContainer;
 import mekanism.common.capabilities.holder.slot.IInventorySlotHolder;
 import mekanism.common.tile.base.TileEntityMekanism;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
 
 final class MeAdvancedFactorySupport {
     private MeAdvancedFactorySupport() {
     }
 
-    interface Owner extends MeMoreMachineFactoryAeMachine {
-        List<IInventorySlot> meInputSlots();
-
-        List<IInventorySlot> meOutputSlots();
-
-        void unpauseRecipeMonitors();
+    interface Owner extends MeMoreMachineFactoryAeMachine, MeExternalFactorySupport.Owner {
     }
 
     static IInventorySlotHolder withPatternSlots(IInventorySlotHolder original, Owner owner) {
-        return MeExternalFactorySupport.withPatternSlots(original, new MeExternalFactorySupport.Owner() {
-            @Override public List<IInventorySlot> meInputSlots() { return owner.meInputSlots(); }
-            @Override public List<IInventorySlot> meOutputSlots() { return owner.meOutputSlots(); }
-            @Override public void unpauseRecipeMonitors() { owner.unpauseRecipeMonitors(); }
-            @Override public MeFactoryAeSupport getAeSupport() { return owner.getAeSupport(); }
-            @Override public MeMekanismMachine getMachine() { return owner.getMachine(); }
-            @Override public Level getOwnerLevel() { return owner.getOwnerLevel(); }
-            @Override public List<IPatternDetails> getAvailablePatterns() { return owner.getAvailablePatterns(); }
-            @Override public int getPatternPriority() { return owner.getPatternPriority(); }
-            @Override public boolean pushPattern(IPatternDetails patternDetails, KeyCounter[] inputHolder) { return owner.pushPattern(patternDetails, inputHolder); }
-            @Override public boolean isBusy() { return owner.isBusy(); }
-            @Nullable @Override public appeng.api.networking.IGridNode getGridNode(Direction dir) { return owner.getGridNode(dir); }
-        });
+        return MeExternalFactorySupport.withPatternSlots(original, owner);
     }
 
     static boolean pushSingleItem(Owner owner, KeyCounter[] inputHolder) {
-        return MeExternalFactorySupport.pushSingleItem(adapt(owner), inputHolder);
+        return MeExternalFactorySupport.pushSingleItem(owner, inputHolder);
     }
 
     static boolean pushSingleItem(Owner owner, IPatternDetails patternDetails, KeyCounter[] inputHolder) {
-        return MeExternalFactorySupport.pushSingleItem(adapt(owner), patternDetails, inputHolder);
+        return MeExternalFactorySupport.pushSingleItem(owner, patternDetails, inputHolder);
     }
 
     static boolean pushItemChemical(Owner owner, KeyCounter[] inputHolder, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushItemChemical(adapt(owner), inputHolder, chemicalTank);
+        return MeExternalFactorySupport.pushItemChemical(owner, inputHolder, chemicalTank);
     }
 
     static boolean pushItemChemical(Owner owner, IPatternDetails patternDetails, KeyCounter[] inputHolder, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushItemChemical(adapt(owner), patternDetails, inputHolder, chemicalTank);
+        return MeExternalFactorySupport.pushItemChemical(owner, patternDetails, inputHolder, chemicalTank);
     }
 
     static boolean pushChemical(Owner owner, KeyCounter[] inputHolder, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushChemical(adapt(owner), inputHolder, chemicalTank);
+        return MeExternalFactorySupport.pushChemical(owner, inputHolder, chemicalTank);
     }
 
     static boolean pushChemical(Owner owner, IPatternDetails patternDetails, KeyCounter[] inputHolder, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushChemical(adapt(owner), patternDetails, inputHolder, chemicalTank);
+        return MeExternalFactorySupport.pushChemical(owner, patternDetails, inputHolder, chemicalTank);
     }
 
     static boolean pushChemical(Owner owner, KeyCounter[] inputHolder, java.util.List<mekanism.api.chemical.IChemicalTank> chemicalTanks) {
-        return MeExternalFactorySupport.pushChemical(adapt(owner), inputHolder, chemicalTanks);
+        return MeExternalFactorySupport.pushChemical(owner, inputHolder, chemicalTanks);
     }
 
     static boolean pushChemical(Owner owner, IPatternDetails patternDetails, KeyCounter[] inputHolder, java.util.List<mekanism.api.chemical.IChemicalTank> chemicalTanks) {
-        return MeExternalFactorySupport.pushChemical(adapt(owner), patternDetails, inputHolder, chemicalTanks);
+        return MeExternalFactorySupport.pushChemical(owner, patternDetails, inputHolder, chemicalTanks);
     }
 
     static boolean pushFluidChemical(Owner owner, KeyCounter[] inputHolder, mekanism.api.fluid.IExtendedFluidTank fluidTank, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushFluidChemical(adapt(owner), inputHolder, fluidTank, chemicalTank);
+        return MeExternalFactorySupport.pushFluidChemical(owner, inputHolder, fluidTank, chemicalTank);
     }
 
     static boolean pushFluidChemical(Owner owner, IPatternDetails patternDetails, KeyCounter[] inputHolder, mekanism.api.fluid.IExtendedFluidTank fluidTank, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushFluidChemical(adapt(owner), patternDetails, inputHolder, fluidTank, chemicalTank);
+        return MeExternalFactorySupport.pushFluidChemical(owner, patternDetails, inputHolder, fluidTank, chemicalTank);
     }
 
     static boolean pushFluidChemical(Owner owner, KeyCounter[] inputHolder, mekanism.api.fluid.IExtendedFluidTank fluidTank, java.util.List<mekanism.api.chemical.IChemicalTank> chemicalTanks) {
-        return MeExternalFactorySupport.pushFluidChemical(adapt(owner), inputHolder, fluidTank, chemicalTanks);
+        return MeExternalFactorySupport.pushFluidChemical(owner, inputHolder, fluidTank, chemicalTanks);
     }
 
     static boolean pushFluidChemical(Owner owner, IPatternDetails patternDetails, KeyCounter[] inputHolder, mekanism.api.fluid.IExtendedFluidTank fluidTank, java.util.List<mekanism.api.chemical.IChemicalTank> chemicalTanks) {
-        return MeExternalFactorySupport.pushFluidChemical(adapt(owner), patternDetails, inputHolder, fluidTank, chemicalTanks);
+        return MeExternalFactorySupport.pushFluidChemical(owner, patternDetails, inputHolder, fluidTank, chemicalTanks);
     }
 
     static boolean pushItemFluidChemical(Owner owner, KeyCounter[] inputHolder, mekanism.api.fluid.IExtendedFluidTank fluidTank, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushItemFluidChemical(adapt(owner), inputHolder, fluidTank, chemicalTank);
+        return MeExternalFactorySupport.pushItemFluidChemical(owner, inputHolder, fluidTank, chemicalTank);
     }
 
     static boolean pushItemFluidChemical(Owner owner, IPatternDetails patternDetails, KeyCounter[] inputHolder, mekanism.api.fluid.IExtendedFluidTank fluidTank, mekanism.api.chemical.IChemicalTank chemicalTank) {
-        return MeExternalFactorySupport.pushItemFluidChemical(adapt(owner), patternDetails, inputHolder, fluidTank, chemicalTank);
+        return MeExternalFactorySupport.pushItemFluidChemical(owner, patternDetails, inputHolder, fluidTank, chemicalTank);
     }
 
     static boolean updateServer(Owner owner, boolean sendUpdatePacket) {
@@ -122,7 +102,7 @@ final class MeAdvancedFactorySupport {
     }
 
     static boolean updateServer(Owner owner, boolean sendUpdatePacket, mekanism.api.fluid.IExtendedFluidTank outputTank) {
-        return MeExternalFactorySupport.updateServer(adapt(owner), sendUpdatePacket, outputTank);
+        return MeExternalFactorySupport.updateServer(owner, sendUpdatePacket, outputTank);
     }
 
     static void createNodeOnFirstTick(TileEntityMekanism tile, MeFactoryAeSupport support, Level level, BlockPos pos) {
@@ -137,22 +117,6 @@ final class MeAdvancedFactorySupport {
         MeExternalFactorySupport.load(support, tag, registries);
     }
 
-    private static MeExternalFactorySupport.Owner adapt(Owner owner) {
-        return new MeExternalFactorySupport.Owner() {
-            @Override public List<IInventorySlot> meInputSlots() { return owner.meInputSlots(); }
-            @Override public List<IInventorySlot> meOutputSlots() { return owner.meOutputSlots(); }
-            @Override public void unpauseRecipeMonitors() { owner.unpauseRecipeMonitors(); }
-            @Override public MeFactoryAeSupport getAeSupport() { return owner.getAeSupport(); }
-            @Override public MeMekanismMachine getMachine() { return owner.getMachine(); }
-            @Override public Level getOwnerLevel() { return owner.getOwnerLevel(); }
-            @Override public List<IPatternDetails> getAvailablePatterns() { return owner.getAvailablePatterns(); }
-            @Override public int getPatternPriority() { return owner.getPatternPriority(); }
-            @Override public boolean pushPattern(IPatternDetails patternDetails, KeyCounter[] inputHolder) { return owner.pushPattern(patternDetails, inputHolder); }
-            @Override public boolean isBusy() { return owner.isBusy(); }
-            @Nullable @Override public appeng.api.networking.IGridNode getGridNode(Direction dir) { return owner.getGridNode(dir); }
-        };
-    }
-
     static List<IInventorySlot> noItemOutput() {
         return Collections.emptyList();
     }
@@ -162,3 +126,4 @@ final class MeAdvancedFactorySupport {
         return MeFactoryAeSupport.withAeRecipeEnergy(owner, energyContainer, cachedRecipe);
     }
 }
+
